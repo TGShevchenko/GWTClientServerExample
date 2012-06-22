38caaaf13550675ba03a935cfd4cef10bb647ebb
taras <taras@taras-mlpt.(none)>
2012-06-22 22:57:26 +0300

    Updating sources

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tstudio::Application.config.secret_token = '9d367a1be6c5e553ca74af55162c8c26fa020a760ea203861ddf9fadbba98b678bfaab4d770d8bac273d607c29aaad1d63871f52cd8277e58f069a1a14a86847'
